e57f2fb30d81d4c11d426729189ceb7d7243f9ba
allnovice <zepolopmacosusejcire@gmail.com>
2025-08-04 21:43:58 +0800

    Update create_tables.sql

--- snowflake/create_tables.sql
+++ snowflake/create_tables.sql
@@ -1,1 +1,15 @@
-samoke
+CREATE TABLE Departments (
+  DepartmentID INT,
+  DepartmentName STRING
+);
+
+CREATE TABLE Asset_Transactions (
+  TransactionID INT,
+  AssetID INT,
+  DepartmentID INT,
+  VendorID INT,
+  LocationID INT,
+  DateID INT,
+  Cost FLOAT
+);
+

0919f730d5b75aa971597aa872021838a4d28af4
allnovice <zepolopmacosusejcire@gmail.com>
2025-08-04 21:44:32 +0800

    Update transformations.sql

--- snowflake/transformations.sql
+++ snowflake/transformations.sql
@@ -1,1 +1,10 @@
-sample
+-- Example: Join Assets with Departments
+SELECT 
+  a.AssetID, 
+  a.AssetName, 
+  d.DepartmentName, 
+  t.Cost
+FROM Asset_Transactions t
+JOIN Departments d ON t.DepartmentID = d.DepartmentID
+JOIN Assets a ON t.AssetID = a.AssetID;
+
